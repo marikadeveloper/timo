92adbe5421bdf13c8c0b4db7869dd18bfe5bb6e8
Marika Mella <marika.developer@gmail.com>
2024-09-27 15:18:13 +0200

    Refactor Task component to remove commented out code for stopping a task

--- src/components/tasks/task/index.tsx
+++ src/components/tasks/task/index.tsx
@@ -14,12 +14,6 @@ const Task: React.FC<TaskProps> = ({ task }) => {
       <p>{task.code}</p>
       <p>{task.description}</p>
       <p>{getTaskDurationString(task.timers)}</p>
-      {/* <IconButton
-        iconName='stop-circle'
-        ariaLabel='Stop task'
-        iconAriaLabel='Stop icon'
-        onClick={() => console.log('Stop task')}
-      /> */}
     </Card>
   );
 };
